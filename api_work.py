89262ec8524d39134f2336b69643e903e50e6eb4
BarabanovaIrina <irlania@mail.ru>
2021-01-15 18:52:55 +0300

    fix api work

--- api_work.py
+++ api_work.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-pulls_response = requests.get('https://api.github.com/repos/nccr-itmo/FEDOT/pulls')
+pulls_response = requests.get('https://api.github.com/repos/BarabanovaIrina/MentalArithmeticTask/pulls')
 pulls_list = json.loads(pulls_response.text)
 pulled_branches = [pull['head']['ref'] for pull in pulls_list]
 
